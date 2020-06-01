44ce2548bf560ded23b7f3914da405906913343f
YI LUO <5194080+yiloo@users.noreply.github.com>
2020-05-31 21:03:14 -0500

    change configuration file

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, url_for, redirect
 from flask import render_template
+import json
+
 app = Flask(__name__, static_url_path='', static_folder='static/')
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+# read configurations
+with open('config.json') as json_file:
+	configs = json.load(json_file)
+	app.config['title'] = configs['website']['title']
+	app.config['data_base_info'] = configs['database']
 
 @app.route('/')
 def index():
