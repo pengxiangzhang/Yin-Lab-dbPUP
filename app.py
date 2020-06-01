49f395d13746d71f145f94de00761ea600756fa2
YI LUO <5194080+yiloo@users.noreply.github.com>
2020-06-01 00:31:18 -0500

    add markdown support

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, url_for, redirect
 from flask import render_template
 import json
-
+from flaskext.markdown import Markdown
 app = Flask(__name__, static_url_path='/static')
+md = Markdown(app, extensions=['fenced_code'])
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 # read configurations
@@ -13,7 +14,8 @@ with open('config.json') as json_file:
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	c = open('content/about.md', 'r').read()
+	return render_template('index.html', content = c)
 
 @app.route('/work')
 def work():
