b2238683c3fdeefcdeca8da814af26f0abaadca0
YI LUO <5194080+yiloo@users.noreply.github.com>
2020-06-01 16:53:54 -0500

    add record module

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, url_for, redirect
 from flask import render_template
 from flaskext.markdown import Markdown
 from flask_sqlalchemy import SQLAlchemy
+from models.record import Record
 import json
 
 # Application configurations
@@ -29,28 +30,16 @@ with open('config.json') as json_file:
 dtbs = SQLAlchemy(app)
 
 # routing
-class Record(dtbs.Model):
-	__tablename__ = 'characterize'
-	db = dtbs.Column(dtbs.String(30))
-	uniq_id = dtbs.Column(dtbs.String(30), primary_key=True)
-	entry = dtbs.Column(dtbs.String(30))
-	protein = dtbs.Column(dtbs.String(30))
-	organism = dtbs.Column(dtbs.String(40))
-	organism_id = dtbs.Column(dtbs.Integer)
-	gene = dtbs.Column(dtbs.String(30))
-	protein_exist = dtbs.Column(dtbs.Integer)
-	seq_versio = dtbs.Column(dtbs.Integer)
-	seq = dtbs.Column(dtbs.VARCHAR(500))
 
 @app.route('/')
 def index():
 	c = open('content/about.md', 'r').read()
 	return render_template('index.html', content = c)
 
-
 @app.route('/work')
 def work():
-	return render_template('work.html')
+	records = Record.query.all()
+	return render_template('work.html', records = records)
 
 @app.route('/user/<username>/<firstname>')
 def newUser(username, firstname):
