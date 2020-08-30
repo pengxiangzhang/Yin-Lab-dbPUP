85f9ec33162fb2092e26e3eb83997fc5c6181986
Yhe <yhe@huskers.unl.edu>
2020-08-30 13:58:57 -0500

    change the attribute name from genename to database in db, models, template

--- models/swiRecord.py
+++ models/swiRecord.py
@@ -6,7 +6,7 @@ class SwiRecord(dtbs.Model):
         __tablename__= 'swiss'
         protein_enzyme = dtbs.Column(dtbs.String(30))
         strain=dtbs.Column(dtbs.String(30))
-        gene_name=dtbs.Column(dtbs.String(30))
+        database=dtbs.Column(dtbs.String(30))
         uniq_id=dtbs.Column(dtbs.String(30),primary_key=True)
         family=dtbs.Column(dtbs.String(30))
         ec=dtbs.Column(dtbs.String(30))
