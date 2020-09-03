bbb1509b985555d015b79e5ec3091f9fb203de05
Pengxiang Zhang <z-git@protonmail.com>
2020-09-02 19:26:31 -0500

    Fix bug on database of naming error

--- models/charRecord.py
+++ models/charRecord.py
@@ -4,21 +4,23 @@ dtbs = SQLAlchemy()
 
 class CharRecord(dtbs.Model):
 	__tablename__ = 'characterize'
-	number = dtbs.Column(dtbs.Integer)
+	number = dtbs.Column(dtbs.VARCHAR(5))
 	strain = dtbs.Column(dtbs.VARCHAR(60))
 	protein_name = dtbs.Column(dtbs.VARCHAR(60))
 	gene_name = dtbs.Column(dtbs.VARCHAR(30))
 	uniq_id = dtbs.Column(dtbs.VARCHAR(30), primary_key=True)
 	ec = dtbs.Column(dtbs.VARCHAR(50))
 	substrate = dtbs.Column(dtbs.VARCHAR(60))
-	production = dtbs.Column(dtbs.VARCHAR(60))
-	pdb = dtbs.Column(dtbs.VARCHAR(30))
-	km = dtbs.Column(dtbs.Float)
-	vmax = dtbs.Column(dtbs.Float)
-	kcat = dtbs.Column(dtbs.Float)
-	doi = dtbs.Column(dtbs.VARCHAR(50))
+	product = dtbs.Column(dtbs.VARCHAR(60))
+	pdb = dtbs.Column(dtbs.VARCHAR(60))
+	km = dtbs.Column(dtbs.VARCHAR(10))
+	vmax = dtbs.Column(dtbs.VARCHAR(10))
+	kcat = dtbs.Column(dtbs.VARCHAR(10))
 	family = dtbs.Column(dtbs.VARCHAR(10))
 	seq = dtbs.Column(dtbs.VARCHAR(1000))
-
-
-
+	doi = dtbs.Column(dtbs.VARCHAR(50))
+	tax_id = dtbs.Column(dtbs.VARCHAR(10))
+	type=dtbs.Column(dtbs.VARCHAR(10))
+	pubchem_s=dtbs.Column(dtbs.VARCHAR(300))
+	pubchem_p=dtbs.Column(dtbs.VARCHAR(300))
+	
