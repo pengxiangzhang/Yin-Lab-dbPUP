3b55613bde6f805caae780954f0102f3da44feb0
Pengxiang Zhang <z-git@protonmail.com>
2020-10-20 14:52:13 -0500

    Move class list out to app.py

--- app.py
+++ app.py
@@ -108,31 +108,16 @@ def download():
     return render_template('main.html', content=to_md(c), description="", title=title, name=name)
 
 
-@app.route('/dbpup/evidence/<family_id>')
-def evidence(family_id):
+@app.route('/dbpup/evidence/all')
+def evidence():
     title = "Evidence - "
     name = "Evidence"
-    if family_id == 'all':
-        records = charRecord.CharRecord.query.all()
-        data_analyzer = Data_analyzer(records)
-        ec_link, pdb_row = data_analyzer.ec_pdb_split()
-        sub, prod = data_analyzer.substrate_product_split()
+    records = charRecord.CharRecord.query.all()
+    data_analyzer = Data_analyzer(records)
+    ec_link, pdb_row = data_analyzer.ec_pdb_split()
+    sub, prod = data_analyzer.substrate_product_split()
 
-    else:
-        records = charRecord.CharRecord.query.filter_by(family=family_id)
-        data_analyzer = Data_analyzer(records)
-        ec_link, pdb_row = data_analyzer.ec_pdb_split()
-        sub, prod = data_analyzer.substrate_product_split()
-
-    found = False
-    for record in records:
-        found = True
-        break
-    if not found:
-        abort(404)
-
-    return render_template("evidence.html", records=records, rows=pdb_row, ec=ec_link, sub=sub, product=prod,
-                           description="", title=title, name=name)
+    return render_template("evidence.html", records=records, rows=pdb_row, ec=ec_link, sub=sub, product=prod,description="", title=title, name=name)
 
 
 @app.route('/dbpup/swissport/<family_id>', methods=['GET', 'POST'])
@@ -224,6 +209,8 @@ def family(family_id):
     title = "Family - " + family_id + " - "
     amount = 0
     name = ""
+    if not is_family(family_id):
+        abort(404)
     try:
         with open('content/family_' + family_id + '.md') as c:
             name = get_title(c)
@@ -232,21 +219,11 @@ def family(family_id):
     except Exception:
         c = open('content/nothing.md', 'r').read()
         name = "null"
-        # abort(404)
-        # TODO: Delete everything with pass
 
-    if family_id == 'OR1':
-        pass
-    elif family_id == 'OR2':
-        pass
-    elif family_id == 'OR3':
-        pass
-    elif family_id == 'OR4':
+    if family_id == 'OR4':
         amount = 4
     elif family_id == 'OR5':
         amount = 12
-    elif family_id == 'OR6':
-        pass
     elif family_id == 'OR7':
         amount = 5
     elif family_id == 'OR8':
@@ -255,14 +232,10 @@ def family(family_id):
         amount = 7
     elif family_id == 'FR1':
         amount = 4
-    elif family_id == 'FR2':
-        pass
     elif family_id == 'FR3':
         amount = 4
     elif family_id == 'FR4':
         amount = 4
-    elif family_id == 'HR1':
-        pass
     elif family_id == 'HR2':
         amount = 3
     elif family_id == 'HR3':
@@ -271,27 +244,15 @@ def family(family_id):
         amount = 11
     elif family_id == 'HR5':
         amount = 5
-    elif family_id == 'HR6':
-        pass
     elif family_id == 'HR7':
         amount = 5
     elif family_id == 'HR8':
         amount = 6
-    elif family_id == 'NCR1':
-        pass
-    elif family_id == 'IR1':
-        pass
-    elif family_id == 'IR2':
-        pass
     elif family_id == 'UC1':
         amount = 13
     elif family_id == 'UC2':
         amount = 8
-    else:
-        abort(404)
-
-    return render_template('family.html', family_id=family_id, amount=amount, content=to_md(c), description="",
-                           title=title, name=name)
+    return render_template('family.html', family_id=family_id, amount=amount, content=to_md(c), description="",title=title, name=name)
 
 
 @app.route("/dbpup/subfamily/<family_id>")
@@ -330,15 +291,37 @@ def network(family_id):
 def classes(class_id):
     title = "Classes - " + class_id + " - "
     name = ""
+    amount = 0
     try:
         with open('content/class_' + class_id + '.md') as c:
             name = get_title(c)
             next(c)
             c = c.read()
     except Exception:
+        c = open('content/nothing.md', 'r').read()
+        name = "null"
+    
+    if class_id == 'ORs':
+        amount = 9
+    elif class_id == 'FRs':
+        amount = 4
+    elif class_id == 'HRs':
+        amount = 8
+    elif class_id == 'NCRs':
+        amount = 1
+    elif class_id == 'IRs':
+        amount = 2
+    elif class_id == 'SRs':
+        amount = 0
+    elif class_id == 'TRs':
+        amount = 0
+    elif class_id == 'UCs':
+        amount = 2
+    else:
         abort(404)
+    
 
-    return render_template('classes.html', class_id=class_id, content=to_md(c), description="", title=title, name=name)
+    return render_template('classes.html', class_id=class_id, content=to_md(c), description="", title=title, name=name, amount=amount)
 
 
 @app.route("/dbpup/blast", methods=["GET", "POST"])
@@ -381,22 +364,20 @@ def inject_now():
 def get_title(file):
     return file.readline()[2:]
 
-
-subfamily_list = ["OR4_1", "OR4_2", "OR4_3", "OR4_4", "OR5_1", "OR5_2", "OR5_3", "OR5_4", "OR5_5", "OR5_6", "OR5_7",
-                  "OR5_8", "OR5_9", "OR5_10", "OR5_11", "OR7_1", "OR7_2", "OR7_3", "OR7_4", "OR7_5", "OR8_1", "OR8_2",
-                  "OR8_3", "OR8_4", "OR8_5", "OR8_6", "OR8_7", "OR8_8", "OR8_9", "OR8_10", "OR8_11", "OR8_12", "OR8_13",
-                  "OR8_14", "OR9_1", "OR9_2", "OR9_3", "OR9_4", "OR9_5", "OR9_5", "OR9_6", "OR9_7", "FR1_1", "FR1_2",
-                  "FR1_3", "FR1_4", "FR3_1", "FR3_2", "FR3_3", "FR3_4", "FR4_1", "FR4_2", "FR4_3", "FR4_4", "HR2_1",
-                  "HR2_2", "HR2_3", "HR3_1", "HR3_2", "HR3_3", "HR3_4", "HR3_5", "HR3_6", "HR3_7", "HR3_8", "HR4_1",
-                  "HR4_2", "HR4_3", "HR4_4", "HR4_5", "HR4_6", "HR4_7", "HR4_8", "HR4_9", "HR5_1", "HR5_2", "HR5_3",
-                  "HR5_4", "HR5_5", "HR7_1", "HR7_2", "HR7_3", "HR7_4", "HR7_5", "HR8_1", "HR8_2", "HR8_3", "HR8_4",
-                  "HR8_5", "HR8_6", "UC1_1", "UC1_2", "UC1_3", "UC1_4", "UC1_5", "UC1_6", "UC1_7", "UC1_8", "UC1_9",
-                  "UC1_10", "UC1_11", "UC1_12", "UC1_13", "UC2_1", "UC2_2", "UC2_3", "UC2_4", "UC2_5", "UC2_6", "UC2_7",
-                  "UC2_8"]
-
-
+subfamilyfile = open("static/materials/subfamily.txt", "r")
+subfamily_list=(subfamilyfile.readline().split())
+subfamilyfile.close()
+familyfile = open("static/materials/family.txt", "r")
+family_list=(familyfile.readline().split())
+familyfile.close()
 def is_subfamily(family_id):
     if family_id in subfamily_list:
+        return True
+    else:
+        return False
+        
+def is_family(family_id):
+    if family_id in family_list:
         return True
     else:
         return False
